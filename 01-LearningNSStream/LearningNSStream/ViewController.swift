589b1fb70b3ce5cd89d9b514c7ed31249be42999
Or Ron <or.ron10@gmail.com>
2016-09-06 21:00:32 +0300

    Started project 01 working with NSStream

---
+++ 01-LearningNSStream/LearningNSStream/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearningNSStream
+//
+//  Created by Or on 06/09/2016.
+//  Copyright © 2016 Or. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
